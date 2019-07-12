0e92171bbd6ed052c868ae41ab9ddaba5561c340
Siarhei Ladutska <siarhei_ladutska@epam.com>
2019-07-12 15:12:25 +0300

    remove ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,16 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigService } from './config/config.service';
 import { IngredientsModule } from './ingredients/ingredients.module';
 import { RecipesModule } from './recipes/recipes.module';
 import { CategoriesModule } from './categories/categories.module';
-
-const configService = new ConfigService(`.env`);
-const dbUri = configService.get('DB_URI');
 
 @Module({
   imports: [
     IngredientsModule,
     CategoriesModule,
     RecipesModule,
-    MongooseModule.forRoot(dbUri, { useNewUrlParser: true }),
+    MongooseModule.forRoot(process.env.DB_URI, { useNewUrlParser: true }),
   ],
   controllers: [AppController],
   providers: [AppService],
